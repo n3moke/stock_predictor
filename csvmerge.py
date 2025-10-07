9735e59a50f22100befa46348b6625b0bcca58f9
kiluxx <karsten.schnitzler@gmail.com>
2025-10-07 14:30:20 +0200

    fixed bug in csvmerge

--- csvmerge.py
+++ csvmerge.py
@@ -3,23 +3,23 @@ import glob
 
 def merge_csv_With_path_andTimeframe(timeframe: int,stockName: str ,llmName: str,method:str):
 
-    csv_files = glob.glob(f'results/{stockName}/{llmName}/{timeframe}m/*.csv')
+    csv_files = glob.glob(f'results/{stockName}/{timeframe}M/*.csv')
     merged = ""
     dfs = []
+    print(csv_files)
     for idx, file in enumerate(csv_files):
-        df = pd.read_csv(file)
+        df = pd.read_csv(file, delimiter=';', decimal=',')
         df.rename(columns={'prediction': f'prediction_{idx+1}'}, inplace=True)
         dfs.append(df)
-
+    # print(dfs)
     from functools import reduce
     merged = reduce(lambda left, right: pd.merge(left, right, on='date', how='outer'), dfs)
-    print(merged)
     merged.to_csv(f'results/{stockName}_{llmName}_{timeframe}m_{method}.csv', sep=';', decimal=',', index=False)
 
 
 if __name__ == '__main__': 
     # stockName = 'NVDA'
-    # llm_name = 'llama'
+    llm_name = 'llama'
     method = 'combined'
     # merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=1,llmName=llm_name,method=method)
     # merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=3,llmName=llm_name,method=method)
@@ -30,14 +30,41 @@ if __name__ == '__main__':
     # merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=3,llmName=llm_name,method=method)
     # merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=12,llmName=llm_name,method=method)
     # merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=60,llmName=llm_name,method=method)
+    stockName = 'AAPL'
+    merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=1,llmName=llm_name,method=method)
+    merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=3,llmName=llm_name,method=method)
+    # merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=12,llmName=llm_name,method=method)
+    # merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=60,llmName=llm_name,method=method)
+    # llm_name = 'gemma'
+    # merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=1,llmName=llm_name,method=method)
+    # merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=3,llmName=llm_name,method=method)
+    # merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=12,llmName=llm_name,method=method)
+    # merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=60,llmName=llm_name,method=method)
+
+    stockName = 'TSLA'
+    merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=1,llmName=llm_name,method=method)
+    merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=3,llmName=llm_name,method=method)
+    stockName = 'NVDA'
+    merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=1,llmName=llm_name,method=method)
+    merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=3,llmName=llm_name,method=method)
+    stockName = 'NFLX'
+    merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=1,llmName=llm_name,method=method)
+    merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=3,llmName=llm_name,method=method)
     stockName = 'MSFT'
-    llm_name = 'llama'
     merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=1,llmName=llm_name,method=method)
     merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=3,llmName=llm_name,method=method)
-    merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=12,llmName=llm_name,method=method)
-    merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=60,llmName=llm_name,method=method)
-    llm_name = 'gemma'
+    stockName = 'META'
     merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=1,llmName=llm_name,method=method)
     merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=3,llmName=llm_name,method=method)
-    merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=12,llmName=llm_name,method=method)
-    merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=60,llmName=llm_name,method=method)
+    stockName = 'GOOGL'
+    merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=1,llmName=llm_name,method=method)
+    merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=3,llmName=llm_name,method=method)
+    stockName = 'GOOG'
+    merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=1,llmName=llm_name,method=method)
+    merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=3,llmName=llm_name,method=method)
+    stockName = 'AVGO'
+    merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=1,llmName=llm_name,method=method)
+    merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=3,llmName=llm_name,method=method)
+    stockName = 'AMZN'
+    merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=1,llmName=llm_name,method=method)
+    merge_csv_With_path_andTimeframe(stockName=stockName,timeframe=3,llmName=llm_name,method=method)
